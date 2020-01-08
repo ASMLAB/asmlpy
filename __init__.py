d5440c353f11fb0b2f46e7efe4399210a7d24084
hajsong <hajsong@gmail.com>
2020-01-09 03:30:36 +0900

    interpolation, add function

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from .mitgcmdiag import loadgrid, calmld, computecurl
 from .others import inpaint_nans, inpaint_nans_local, detrend2d
 from .llc90_tools import readllc90
 from .dicdiagtool import advection,hadvection,vadvection
-from .interpolation import interp_nonreg_xy
+from .interpolation import interp_nonreg_xy, interp_nonreg_xyz
 
 __all__ = ['calmld', 'loadgrid', 'computecurl', 
            'inpaint_nans_local', 'inpaint_nans', 'detrend2d']
